445624321b80b970151976424d8ce21706a8b163
Splash1972 <161398773+Splash1972@users.noreply.github.com>
2026-09-17 13:18:36 -0400

    added icon to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,10 @@ export default function Nav() {
 
     return (
         <nav className="nav">
-            <a href="#top" className="nav__brand">melodicode</a>
+            <a href="#top" className="nav__brand">
+                <img src="/mcicon.png" alt="" className="nav__icon" />
+                melodicode
+            </a>
             <div className="nav__links">
                 {links.map((link) => (
                     <a key={link.href} href={link.href} className="nav__link">
